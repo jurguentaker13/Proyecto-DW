d3d75a4ec8f12df0a39bb895646dbf43fd0e81ad
jurguentaker13 <jurguentaker13@gmail.com>
2024-05-25 11:16:21 -0600

    Ejemplo FinalBack

--- src/main/java/com/example/demo/BackendFinalApplication.java
+++ src/main/java/com/example/demo/BackendFinalApplication.java
@@ -18,7 +18,7 @@ public class BackendFinalApplication {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "https://ejercicio20-86165.web.app/")
                 .allowCredentials(true)
                 .allowedHeaders("Origin", "Authorization", "Content-Type")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
