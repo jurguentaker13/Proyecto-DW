d4fe67f6a260e59f608f6ff8149370e7377fd9bd
jurguentaker13 <jurguentaker13@gmail.com>
2024-05-25 12:18:03 -0600

    Ejemplo FinalBack

--- src/main/java/com/example/demo/BackendFinalApplication.java
+++ src/main/java/com/example/demo/BackendFinalApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,9 +21,12 @@ public class BackendFinalApplication {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
                 .allowedOrigins( "https://ejercicio20-86165.web.app/")
-                .allowCredentials(true)
-                .allowedHeaders("Origin", "Authorization", "Content-Type")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedMethods(HttpMethod.GET.name(),
+                                HttpMethod.POST.name(),
+                                HttpMethod.PUT.name(),
+                                HttpMethod.DELETE.name())
+                .allowedHeaders(HttpHeaders.CONTENT_TYPE,
+                        HttpHeaders.AUTHORIZATION);
     }
    };
  }
