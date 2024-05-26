3fe3c277a3a80ebd101586875a8e05fe1b81cfec
jurguentaker13 <jurguentaker13@gmail.com>
2024-05-26 13:51:22 -0600

    Ejemplo FinalBack

--- src/main/java/com/example/demo/BackendFinalApplication.java
+++ src/main/java/com/example/demo/BackendFinalApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class BackendFinalApplication {
+public class BackendFinalApplication {s
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendFinalApplication.class, args);
