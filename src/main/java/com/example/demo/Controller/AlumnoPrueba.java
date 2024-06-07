dd06a5a61e092196b97bb225916f24cd7a0eb89a
jurguentaker13 <jurguentaker13@gmail.com>
2024-06-07 17:04:30 -0600

    Ejemplo FinalBack

--- src/main/java/com/example/demo/Controller/AlumnoPrueba.java
+++ src/main/java/com/example/demo/Controller/AlumnoPrueba.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/Alumno")
+@RequestMapping("/Alumnos")
 public class AlumnoPrueba {
     @Autowired
     ServicioAlumno salumno;
